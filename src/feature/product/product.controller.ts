be823e19cc9178bb216629935409a3b66c81166c
Kingsley Etoka <etokakingsley@gmail.com>
2023-05-16 23:55:29 +0100

    added new product creation

--- src/feature/product/product.controller.ts
+++ src/feature/product/product.controller.ts
@@ -1,18 +1,23 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { ProductModel } from './model/product.model';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { Product } from '@prisma/client';
 
 @Controller('products')
 export class ProductController {
   constructor(private readonly prodService: ProductService) {}
 
   @Get()
-  getAllProducts(): Promise<ProductModel[]> {
+  getAllProducts(): Promise<Product[]> {
     return this.prodService.getAllProducts();
   }
 
   @Get('/:id')
-  getProductById(@Param('id') id: number): Promise<ProductModel> {
+  getProductById(@Param('id') id: number): Promise<Product> {
     return this.prodService.getProductById(id);
+  }
+
+  @Post('/create')
+  addNewProduct(@Body() product: Product) {
+    return this.prodService.addNewProduct(product);
   }
 }
